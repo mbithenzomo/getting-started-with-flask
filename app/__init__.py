ee750709a41561bf6a952e80515617c7f0cda48b
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-11-05 11:21:29 +0300

    Add core files for project

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the views
+from app import views
+
+# Load the config file
+app.config.from_object('config')
